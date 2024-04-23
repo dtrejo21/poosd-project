811145d6ec7c40bde3fa163a0500e308dff0484d
klemon <lennonkiara@gmail.com>
2024-04-22 22:28:55 -0400

    the stuff

--- KnightGO/Assets/Utillities/KGConstants.cs
+++ KnightGO/Assets/Utillities/KGConstants.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class KGConstants
 {
-    public static string SCENE_WORLD = "World";
+    public static string SCENE_WORLD = "TestScene";
     public static string SCENE_CAPTURE = "Capture";
     public static string SCENE_BATTLE = "Battle";
+    public static string SCENE_ABATTLE = "ABattle";
+    public static string SCENE_WIN = "Win";
+    public static string SCENE_LOST = "Lost";
 }
